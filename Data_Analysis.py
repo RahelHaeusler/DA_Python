4016635188c54b4b76d53496283ab9b0c8a7f46d
rahae <rahel.haeusler@students.fhnw.ch>
2024-03-09 15:16:37 +0100

    09.03.24 Update of functions

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -1,19 +1,32 @@
-#inspiraton https://www.kaggle.com/code/janiobachmann/bank-marketing-campaign-opening-a-term-deposit/notebook
+# inspiraton https://www.kaggle.com/code/janiobachmann/bank-marketing-campaign-opening-a-term-deposit/notebook
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# functions to analyze the dataset
+################################
+# for creating a table for statistical numbers
 
-def analyze_csv(file_path):
-    # Display the first few rows of the DataFrame
-    print("First few rows of the data:")
-    print(df.head())
 
+def statistics_csv(df):
     # Display basic statistics of numerical columns
     print("\nBasic statistics of numerical columns:")
     print(df.describe())
+    summary = df.describe()
+    table_html = summary.to_html()
+    # Write the HTML table to a file
+    with open('summary_table.html', 'w') as f:
+        f.write(table_html)
+
+# functions for getting a feeling of the variables
+
+
+def analyze_csv(df):
+    # Display the first few rows of the DataFrame
+    print("First few rows of the data:")
+    print(df.head())
 
     # Display information about the DataFrame including data types and null values
     print("\nInformation about the DataFrame:")
@@ -30,7 +43,7 @@ def analyze_csv(file_path):
         print(f"\nUnique values in column '{col}':")
         print(df[col].unique())
 
-    #show number of occurence per each unique value
+    # show number of occurence per each unique value
     print(df['marital'].value_counts())
     print(df['default'].value_counts())
     print(df['housing'].value_counts())
@@ -41,44 +54,8 @@ def analyze_csv(file_path):
     print(df['previous'].value_counts())
     print(df['poutcome'].value_counts())
 
-    #Histogram for age
-    plt.figure(figsize=(8, 6))
-    plt.hist(df['age'], bins=10, color='skyblue', edgecolor='black')
-    plt.title('Age Distribution')
-    plt.xlabel('Age')
-    plt.ylabel('Frequency')
-    plt.grid(True)
-    plt.show()
 
-    #Histogram for month
-    plt.figure(figsize=(8, 6))
-    plt.hist(df['month'], bins=10, color='skyblue', edgecolor='black')
-    plt.title('Month Distribution')
-    plt.xlabel('Month')
-    plt.ylabel('Frequency')
-    plt.grid(True)
-    plt.show()
-
-    #Histogram for day of week
-    plt.figure(figsize=(8, 6))
-    plt.hist(df['day_of_week'], bins=10, color='skyblue', edgecolor='black')
-    plt.title('Day of Week Distribution')
-    plt.xlabel('Day of Week')
-    plt.ylabel('Frequency')
-    plt.grid(True)
-    plt.show()
-
-    #Histogram for campaign
-    plt.figure(figsize=(8, 6))
-    plt.hist(df['campaign'], bins=10, color='skyblue', edgecolor='black')
-    plt.title('Campaign Distribution')
-    plt.xlabel('Campaign')
-    plt.ylabel('Number')
-    plt.grid(True)
-    plt.show()
-
-
-def visualize_csv(file_path):
+def visualize_csv(df):
     # Finding the unique values of y
     df['y'].unique()
     print(df['y'].unique())
@@ -94,6 +71,7 @@ def visualize_csv(file_path):
     df.hist(bins=20, figsize=(14, 10), color='#E14906')
     plt.show()
 
+
 def scatterplot(df):
     # Scatter Plot for age and subscription to fixed term deposit
     plt.figure(figsize=(8, 6))
@@ -104,14 +82,58 @@ def scatterplot(df):
     plt.grid(True)
     plt.show()
 
+
+def histogramplot(df):
+    # Histogram for age
+    plt.figure(figsize=(8, 6))
+    plt.hist(df['age'], bins=10, color='skyblue', edgecolor='black')
+    plt.title('Age Distribution')
+    plt.xlabel('Age')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    plt.show()
+
+    # Histogram for month
+    plt.figure(figsize=(8, 6))
+    plt.hist(df['month'], bins=10, color='skyblue', edgecolor='black')
+    plt.title('Month Distribution')
+    plt.xlabel('Month')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    plt.show()
+
+    # Histogram for day of week
+    plt.figure(figsize=(8, 6))
+    plt.hist(df['day_of_week'], bins=10, color='skyblue', edgecolor='black')
+    plt.title('Day of Week Distribution')
+    plt.xlabel('Day of Week')
+    plt.ylabel('Frequency')
+    plt.grid(True)
+    plt.show()
+
+    # Histogram for campaign
+    plt.figure(figsize=(8, 6))
+    plt.hist(df['campaign'], bins=10, color='skyblue', edgecolor='black')
+    plt.title('Campaign Distribution')
+    plt.xlabel('Campaign')
+    plt.ylabel('Number')
+    plt.grid(True)
+    plt.show()
+
+
+
 # Provide the file path of the CSV file
 file_path = "marketing.csv"
 # Read the CSV file into a pandas DataFrame with semicolon as delimiter
 df = pd.read_csv(file_path, delimiter=';')
 # Call the function to analyze the CSV file
-analyze_csv(file_path)
-#visualize_csv(file_path)
-#Scatterplot variables towards the Subscription (Y) does not make sense, as there are only two variables
-#for each subscription
-#scatterplot(df)
+# uncomment the line to run the function
+# statistics_csv(df)
+# analyze_csv(df)
+visualize_csv(df)
+# histogramplot(df)
+
+# Scatterplot variables towards the Subscription (Y) does not make sense, as there are only two variables
+# for each subscription
+# scatterplot(df)
 
